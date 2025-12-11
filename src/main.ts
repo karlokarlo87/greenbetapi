2b6557e99b3e9c65d4a476b0675bde4cce6fd861
karlo piranishvili <karlo.piranishvili@gmail.com>
2025-12-11 18:55:57 +0400

    add

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT ?? 3002);
+
+  const corsOptions = {
+    origin: [
+      'http://localhost:3000',
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
+  await app.listen(process.env.PORT || 3002);
 }
 bootstrap();
