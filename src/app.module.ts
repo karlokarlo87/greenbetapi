56e5e68e394d5440c9362f4ed9a707c6b23f0fdf
karlo piranishvili <karlo.piranishvili@gmail.com>
2025-12-16 12:33:52 +0400

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,24 @@ import { SportsModule } from './sports/sports.module';
 import { LeaguesModule } from './leagues/leagues.module';
 import { OddsModule } from './odds/odds.module';
 import { LiveModule } from './live/live.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: +config.get('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // ❗ false in prod
+      }),
+    }),
     ScheduleModule.forRoot(),
     SportsModule, LeaguesModule, OddsModule, LiveModule,
     ],
